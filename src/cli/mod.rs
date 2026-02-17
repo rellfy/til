6c64c7216c370518a2def310470e9a15549726c9
Lorenzo <lorenzo@nioel.com>
2026-02-17 19:36:00 +1000

    chore: adjust command structure

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -9,25 +9,41 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
-    /// Create a new timeline.
-    New { name: String },
+    /// Create a new timeline file.
+    New { file: String },
     /// Manage events.
     Event {
+        /// Path to the .til file.
+        file: String,
         #[command(subcommand)]
         command: EventCommand,
     },
     /// Manage ranges.
     Range {
+        /// Path to the .til file.
+        file: String,
         #[command(subcommand)]
         command: RangeCommand,
     },
     /// Manage tags.
     Tag {
+        /// Path to the .til file.
+        file: String,
         #[command(subcommand)]
         command: TagCommand,
     },
     /// Show the timeline.
-    Show,
+    Show {
+        /// Path to the .til file.
+        file: String,
+    },
+    /// Merge multiple timeline files into one.
+    Merge {
+        /// Path to the output .til file.
+        output: String,
+        /// Paths to the .til files to merge.
+        files: Vec<String>,
+    },
 }
 
 #[derive(Subcommand)]
@@ -38,6 +54,10 @@ enum EventCommand {
     Remove { label: String },
     /// Tag an event.
     Tag { event_label: String, tag: String },
+    /// Remove a tag from an event.
+    Untag { event_label: String, tag: String },
+    /// List all events.
+    List,
 }
 
 #[derive(Subcommand)]
@@ -52,10 +72,16 @@ enum RangeCommand {
     },
     /// Remove a range.
     Remove { label: String },
+    /// List all ranges.
+    List,
 }
 
 #[derive(Subcommand)]
 enum TagCommand {
+    /// Add a tag.
+    Add { label: String },
+    /// Delete a tag and remove it from all events and ranges.
+    Delete { label: String },
     /// List all tags.
     List,
 }
@@ -63,20 +89,26 @@ enum TagCommand {
 impl Cli {
     pub fn run(self) {
         match self.command {
-            Command::New { name } => todo!(),
-            Command::Event { command } => match command {
+            Command::New { file } => todo!(),
+            Command::Event { file, command } => match command {
                 EventCommand::Add { label, datetime } => todo!(),
                 EventCommand::Remove { label } => todo!(),
                 EventCommand::Tag { event_label, tag } => todo!(),
+                EventCommand::Untag { event_label, tag } => todo!(),
+                EventCommand::List => todo!(),
             },
-            Command::Range { command } => match command {
+            Command::Range { file, command } => match command {
                 RangeCommand::Add { label, start, end } => todo!(),
                 RangeCommand::Remove { label } => todo!(),
+                RangeCommand::List => todo!(),
             },
-            Command::Tag { command } => match command {
+            Command::Tag { file, command } => match command {
+                TagCommand::Add { label } => todo!(),
+                TagCommand::Delete { label } => todo!(),
                 TagCommand::List => todo!(),
             },
-            Command::Show => todo!(),
+            Command::Show { file } => todo!(),
+            Command::Merge { output, files } => todo!(),
         }
     }
 }
